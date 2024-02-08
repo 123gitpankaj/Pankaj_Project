2c62b543357b3e05afed0fe8eba99a200b1aae86
pankaj <pankajchaudhari6899@gmail.com>
2024-02-08 14:31:12 +0530

    commit totally

--- Sample/src/Example.java
+++ Sample/src/Example.java
@@ -3,12 +3,6 @@ public class Example {
 	
 	public static void main(String[] args) {
 
-
-		for (int i=0;i<=5;i++){
-
-			System.out.println("Numbers are :"+i);
-
-		}
 		
 		System.out.println("this is kaushal");
 	}
