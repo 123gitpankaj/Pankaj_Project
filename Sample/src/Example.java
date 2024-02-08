e8aaad52c26fc35efbe2b61e0244af353c22280f
pankaj <pankajchaudhari6899@gmail.com>
2024-02-08 14:24:54 +0530

    commit remove

--- Sample/src/Example.java
+++ Sample/src/Example.java
@@ -2,7 +2,13 @@
 public class Example {
 	
 	public static void main(String[] args) {
-		
+
+
+		for (int i=0;i<=5;i++){
+
+			System.out.println("Numbers are :"+i);
+
+		}
 		
 		System.out.println("this is kaushal");
 	}
